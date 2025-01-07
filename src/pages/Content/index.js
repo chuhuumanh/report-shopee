9c5187c4a1fa619c48bab6a42fc1c2a9741326cc
macos <macos@MacBook-Pro-cua-macos.local>
2025-01-07 18:05:35 +0800

    fix issue rwg seated limit

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -5,6 +5,7 @@ const {
   exportWalletTransaction,
   getWalletReportById,
 } = require('./modules/shopee');
+const XLSX = require('xlsx');
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'EXPORT_REPORT') {
@@ -18,7 +19,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 async function getOrdersByRange(startDate, endDate) {
   // const resOrderReport = await requestOrderReport(startDate, endDate);
   // const orderReport = resOrderReport.data;
-  const orderReport = {}
+  const orderReport = {};
   const reportId = orderReport.report_id || 20998824;
   if (reportId) {
     const limit = 3;
@@ -57,9 +58,8 @@ async function getOrdersWalletByRange(startDate, endDate) {
   //   ],
   // });
   // const walletReport = resWalletReport.data.wallet_transaction_report;
-  const walletReport = {}
+  const walletReport = {};
   const reportId = walletReport.report_id || 4153877;
-  console.log(reportId, 'reportId')
 
   if (reportId) {
     const limit = 3;
@@ -68,14 +68,14 @@ async function getOrdersWalletByRange(startDate, endDate) {
     while (!isReportSuccess && retry < limit) {
       try {
         const resReport = await getWalletReportById(reportId);
-        console.log(resReport, 'resReport')
+        console.log(resReport, 'resReport');
         const dataReport = resReport.data.wallet_transaction_report;
         if (dataReport && dataReport.status > 1) {
           isReportSuccess = true;
         }
         retry += 1;
       } catch (error) {
-        console.log(error)
+        console.log(error);
         retry += 1;
       }
     }
@@ -88,9 +88,84 @@ async function getOrdersWalletByRange(startDate, endDate) {
   return [];
 }
 
+function generateTransactionObj(ordersWallet) {
+  const obj = {};
+
+  for (const order of ordersWallet) {
+    const notIntransactionId = ['', 'Mã đơn hàng'];
+    const isHaveTransactionId =
+      order._2 !== null && !notIntransactionId.includes(order._2);
+    if (isHaveTransactionId && order._2) {
+      obj[order._2] = {
+        datePaid: order['Báo cáo'],
+        price: order._4,
+      };
+    }
+  }
+
+  return obj;
+}
+
 async function handleExportReport({ startDate, endDate }) {
   const orders = await getOrdersByRange(startDate, endDate);
-  console.log(orders, 'orders')
   const ordersWallet = await getOrdersWalletByRange(startDate, endDate);
-  console.log(ordersWallet, 'ordersWallet')
+
+  const objTransaction = generateTransactionObj(ordersWallet);
+  const ordersPaid = [];
+  const orderNotPaid = [];
+  for (const order of orders) {
+    const transactionId = order['Mã đơn hàng'];
+
+    const walletTransaction = objTransaction[transactionId];
+    if (walletTransaction) {
+      ordersPaid.push({
+        ['Mã đơn hàng']: order['Mã đơn hàng'],
+        ['Ngày đặt hàng']: order['Ngày đặt hàng'],
+        ['Trạng Thái Đơn Hàng']: order['Trạng Thái Đơn Hàng'],
+        ['Ngày thanh toán']: walletTransaction.datePaid,
+      });
+    } else {
+      orderNotPaid.push({
+        ['Mã đơn hàng']: order['Mã đơn hàng'],
+        ['Ngày đặt hàng']: order['Ngày đặt hàng'],
+        ['Trạng Thái Đơn Hàng']: order['Trạng Thái Đơn Hàng'],
+      });
+    }
+  }
+
+  // Tạo sheet từ dữ liệu ordersPaid và orderNotPaid
+  const wb = XLSX.utils.book_new();
+
+  // Sheet cho các đơn hàng đã thanh toán
+  const wsPaid = XLSX.utils.json_to_sheet(ordersPaid);
+  XLSX.utils.book_append_sheet(wb, wsPaid, 'Orders Paid');
+
+  // Sheet cho các đơn hàng chưa thanh toán
+  const wsNotPaid = XLSX.utils.json_to_sheet(orderNotPaid);
+  XLSX.utils.book_append_sheet(wb, wsNotPaid, 'Orders Not Paid');
+
+  const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+  const blob = new Blob([s2ab(wbout)], { type: 'application/octet-stream' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'Orders_Report.xlsx';
+  document.body.appendChild(a);
+  a.click();
+
+  // Xoá element link sau khi tải
+  document.body.removeChild(a);
+
+  // Giải phóng URL object
+  URL.revokeObjectURL(url);
+}
+
+// Hàm chuyển đổi từ binary string sang ArrayBuffer
+function s2ab(s) {
+  const buf = new ArrayBuffer(s.length);
+  const view = new Uint8Array(buf);
+  for (let i = 0; i < s.length; i++) {
+    view[i] = s.charCodeAt(i) & 0xff;
+  }
+  return buf;
 }
