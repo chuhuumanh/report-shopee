5d5044a7866ad8fa805bb4a788c9a56a671de13c
divmanh <divmanh@divmanhs-MacBook-Pro.local>
2025-01-11 11:31:30 +0800

    inova

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -16,8 +16,11 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  IconButton,
 } from '@mui/material';
 import { getData, updateData } from './firebase';
+import ClearIcon from '@mui/icons-material/Clear';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
 const Popup = () => {
   const [startDate, setStartDate] = useState('2024-12-01');
@@ -30,26 +33,23 @@ const Popup = () => {
 
   // Load API Key từ localStorage khi mở extension
   useEffect(() => {
-    // const storedKey = localStorage.getItem('apiKey');
-    // if (storedKey) {
-    //   checkKeyOnGoogleSheets(storedKey, true);
-    // } else {
-    //   setIsLoading(false);
-    // }
-    setApiKey('fudsfydsu');
-    setIsKeyActive(true);
+    const storedKey = localStorage.getItem('apiKey');
+    if (storedKey) {
+      checkKeyOnGoogleSheets(storedKey, true);
+    } else {
+      setIsLoading(false);
+    }
   }, []);
 
   useEffect(() => {
-    chrome.storage.local.get(['jobIds'], (result) => {
-      const jobIds = result.jobIds || [];
-      if (jobIds.length) {
-        // Đặt một khoảng thời gian để gọi lại API mỗi giây (1000ms)
-        setInterval(() => {
+    setInterval(() => {
+      chrome.storage.local.get(['jobIds'], (result) => {
+        const jobIds = result.jobIds || [];
+        if (jobIds.length) {
           fetchJobStatus(jobIds); // Gọi hàm để kiểm tra trạng thái của các jobIds
-        }, 1000); // 1000ms = 1 giây
-      }
-    });
+        }
+      });
+    }, 1000); // 1000ms = 1 giây
   }, []);
 
   const fetchJobStatus = async (jobIds) => {
@@ -139,8 +139,13 @@ const Popup = () => {
     }
   };
 
+  const handleRemoveJobs = () => {
+    chrome.storage.local.remove(['jobIds'], () => {
+      setJobData([]); // Đặt lại danh sách report (nếu bạn sử dụng state jobData)
+    });
+  };
   return (
-    <Card sx={{ width: '100%', minHeight: 700 }}>
+    <Card sx={{ width: 800, minHeight: 700 }}>
       <CardContent>
         <Box
           sx={{
@@ -170,67 +175,120 @@ const Popup = () => {
             </Typography>
           </Box>
         </Box>
-        <Divider sx={{ marginY: 2 }} />
-
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          <TextField
-            label="Start Date"
-            type="date"
-            InputLabelProps={{ shrink: true }}
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          />
-          <TextField
-            label="End Date"
-            type="date"
-            InputLabelProps={{ shrink: true }}
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-            sx={{ textTransform: 'none', fontWeight: 'bold' }}
-          >
-            Submit
-          </Button>
-        </Box>
 
         <Divider sx={{ marginY: 2 }} />
-        <Typography variant="h6">Link report</Typography>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell>Status</TableCell>
-                <TableCell>Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {jobData.map((job, index) => (
-                <TableRow key={index}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>{job.status}</TableCell>
-                  <TableCell>
-                    {job.fileUrl ? (
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => handleDownload(job.fileUrl)}
-                      >
-                        Download
-                      </Button>
-                    ) : (
-                      'N/A'
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+
+        {!isKeyActive ? (
+          <Box>
+            <Typography variant="h6" sx={{ marginBottom: 2 }}>
+              Vui lòng nhập API Key để kích hoạt
+            </Typography>
+            <TextField
+              label="Nhập API Key"
+              fullWidth
+              variant="outlined"
+              value={apiKey}
+              onChange={(e) => setApiKey(e.target.value)}
+              sx={{ marginBottom: 2 }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleActivateKey}
+              sx={{
+                textTransform: 'none',
+                fontWeight: 'bold',
+              }}
+            >
+              Kích hoạt
+            </Button>
+          </Box>
+        ) : (
+          <Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+              <TextField
+                label="Start Date"
+                type="date"
+                InputLabelProps={{ shrink: true }}
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+              <TextField
+                label="End Date"
+                type="date"
+                InputLabelProps={{ shrink: true }}
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+                sx={{ textTransform: 'none', fontWeight: 'bold' }}
+              >
+                Submit
+              </Button>
+            </Box>
+
+            <Divider sx={{ marginY: 2 }} />
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: '16px',
+              }}
+            >
+              <Typography variant="h6" style={{ flex: 1 }}>
+                Link report
+              </Typography>
+              <IconButton
+                color="error"
+                onClick={handleRemoveJobs}
+                aria-label="Reset reports"
+              >
+                <RemoveCircleOutlineIcon />
+              </IconButton>
+            </div>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Account</TableCell>
+                    <TableCell>Date Export</TableCell>
+                    <TableCell>Status</TableCell>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {jobData.map((job, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>{job.account}</TableCell>
+                      <TableCell>{job.dateReport}</TableCell>
+                      <TableCell>{job.status}</TableCell>
+                      <TableCell>{job.description}</TableCell>
+                      <TableCell>
+                        {job.fileUrl ? (
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => handleDownload(job.fileUrl)}
+                          >
+                            Download
+                          </Button>
+                        ) : (
+                          'N/A'
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+        )}
       </CardContent>
     </Card>
   );
